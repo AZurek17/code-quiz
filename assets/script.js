1ff1b238896e8cea641f90324041a655cbc62ac8
Andy <84168944+AZurek17@users.noreply.github.com>
2023-09-30 21:46:14 -0700

    added functions to js

--- assets/script.js
+++ assets/script.js
@@ -6,17 +6,17 @@ var timer = document.querySelector (".timer");
 var globalIndex = 0;
 var questionText = document.querySelector (".question_text");
 var answerList = document.querySelector (".answer_list");
+var listAnswer = document.querySelector (".list_answer");
 
 quizContainer.style.display = "none";
 endContainer.style.display = "none";
 
 startButton.addEventListener("click", function (e) {
-
 quizContainer.style.display = "block";
-startContainer.style.display = "none"
+startContainer.style.display = "none";
 setTimer();
 display(0);
-} )
+})
 
 var seconds = 60;
 function setTimer() {
@@ -33,12 +33,22 @@ function setTimer() {
 
 var questions = [
     {
-        question:"how many state?",
+        question:"how many states?",
         answer: ["50", "52", "60", "40"], 
         correct_answer: "50",
     },
     {
         question:"how many lakes?",
+        answer: ["50", "52", "60", "40"], 
+        correct_answer: "50",
+    },
+    {
+        question:"how many?",
+        answer: ["50", "52", "60", "40"], 
+        correct_answer: "50",
+    },
+    {
+        question:"how lakes?",
         answer: ["50", "52", "60", "40"], 
         correct_answer: "50",
     }
@@ -50,5 +60,69 @@ function display(GI) {
         var button = document.createElement("button");
         button.textContent = a;
         answerList.appendChild (button)
+        // listAnswer.style.display = "none";
     })
 }
+
+quizContainer.addEventListener("click", function (e) {
+    if (questions.answer !== questions.correct_answer) {        
+        timePenalty();
+        document
+        //questionText.textContent = questions[].question;
+        }else {
+          display();
+            
+        }
+} )
+
+
+function timePenalty() {
+    if (seconds > 4) {
+        seconds - 4;   
+    }else {
+        testOver();
+    }
+}
+
+// var seconds = 60;
+// function setTimer() {
+//     var timerInterval = setInterval(function() {
+//         seconds--;
+//         timer.textContent = seconds + " seconds left.";
+    
+//         if(seconds === 0) {
+//           clearInterval(timerInterval);
+//         //   sendMessage();
+//         }
+//       }, 1000);
+
+
+
+
+function testOver() {{
+    
+}
+
+}
+
+
+// startButton.addEventListener("click", function (e) {
+//     quizContainer.style.display = "block";
+//     startContainer.style.display = "none"
+//     setTimer();
+//     display(0);
+//     })
+
+    
+// incrementEl.addEventListener("click", function(e) {
+//     console.log(e);
+//     count = count + 1;
+//     // count++;
+//     setCounterText();
+//     // countEl.textContent = count;
+//   });
+
+function sendMessage() {
+
+}
+
