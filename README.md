25a134e7cc254447a9a42b8a8235aabc60a84494
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-02 20:59:54 -0700

    added questions,  added README

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # code-quiz
+
+[Visit the Deployed Site](https://azurek17.github.io/code-quiz/)
+
+![code-quiz](Screenshot.png)
+
+### Technoloy used:
+* JavaScript - If...Else - https://www.w3schools.com/jsref/jsref_if.asp
+* JavaScript - Logical Operators - https://www.w3schools.com/java/java_operators.asp
+* JavaScript - For Loop - https://www.w3schools.com/java/java_for_loop.asp
+* JavaScript - timerIntervals - https://www.w3schools.com/js/js_timing.asp
+* Web API's - Event Listeners - https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+
+## Description
+
+A timed Quiz.
+
+## Usage
+
+When you go to the website, you are prompt to "Start Quiz".  Then the user clicks the "Start Quiz" the following occurs:
+
+A timer start and starts to count down.  The first question shows, with four answers to choose from.  If you select an incorrect number. Time is deducted from the clock.
+if the answer is correct, then the second questions prompts.  i
+
+The Quiz is over when you answer all the questions correctly, or you run out of time.
+
+You will then enter you initials for the highScore display.
+
+
+Below are a few examples of code:
+
+
+### Event Listner:
+
+
+answerList.addEventListener("click", function (event) {
+    if (questions.answer === questions.correct_answer) {  
+        seconds = seconds - 8;
+        } else { 
+        quizContainer.style.display = "none";
+        quizContainer1.style.display = "block";
+        display1(0);
+        } 
+    })
+
+
+### Timer Countdown
+
+
+function setTimer() {
+    var timerInterval = setInterval(function() {
+        seconds--;
+        timer.textContent = seconds + " seconds left.";
+
+    }, 1000);      
+}
+
+
+
+
+## Credits
+* Tutoring Session: Abdullah Al Hilfi (via Calendly)
+
+----------------------------------------------------------------------------------
+© 2023 github.com/AZurek17/quiz-code Confidential and Proprietary. All Rights Reserved.
--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
 var startContainer = document.querySelector (".start_container");
 var quizContainer = document.querySelector (".quiz_container");
 var quizContainer1 = document.querySelector (".quiz_container1");
-
+var quizContainer2 = document.querySelector (".quiz_container2");
+var quizContainer3 = document.querySelector (".quiz_container3");
 var endContainer = document.querySelector (".end_container");
 var scoreContainer = document.querySelector (".score_container");
 
 /////////startContainer--------------------
 var startButton = document.querySelector (".start_button");
 var timer = document.querySelector (".timer");
-var globalIndex = 0;
-////////quizContainer------------------------
+var GI = 0;
+////////quizContainer()------------------------
 var questionText = document.querySelector (".question_text");
+var questionText1 = document.querySelector (".question_text1");
+var questionText2 = document.querySelector (".question_text2");
+var questionText3 = document.querySelector (".question_text3");
 var answerList = document.querySelector (".answer_list");
+var answerList1 = document.querySelector (".answer_list1");
+var answerList2 = document.querySelector (".answer_list2");
+var answerList3 = document.querySelector (".answer_list3");
 var listAnswer = document.querySelector (".list_answer");
+var listAnswer1 = document.querySelector (".list_answer1");
+var listAnswer2 = document.querySelector (".list_answer2");
+var listAnswer3 = document.querySelector (".list_answer3");
 ///////end container----------------------
 var listText = document.getElementById ("#list-text");
 var saveButton = document.getElementById("#save-button");
@@ -23,22 +33,15 @@ var savedCount = document.getElementById("#saved-count");
 var savedName = document.getElementById("#saved-name");
 var savedScore = document.getElementById("#saved-score");
 var savedList = document.getElementById("#saved-list");
-
-////--------------------------------
 ////---------------------------------
+
 //// start of code
 quizContainer.style.display = "none";
+quizContainer1.style.display = "none";
+quizContainer2.style.display = "none";
+quizContainer3.style.display = "none";
 endContainer.style.display = "none";
 scoreContainer.style.display = "none";
-
-//button-------event
-startButton.addEventListener("click", function (e) {
-quizContainer.style.display = "block";
-startContainer.style.display = "none";
-
-setTimer();
-display(0);
-})
 
 //---------------timer
 var seconds = 60;
@@ -48,16 +51,19 @@ function setTimer() {
         seconds--;
         timer.textContent = seconds + " seconds left.";
     
-        if (seconds === 0 || seconds < 0) {
+        if (seconds <= 0) {
           clearInterval(timerInterval);
           quizContainer.style.display = "none";
+          quizContainer1.style.display = "none";
+          quizContainer2.style.display = "none";
+          quizContainer3.style.display = "none";
           endContainer.style.display = "block";
           //playerScores()
         }
     }, 1000);      
 }
 
-//-----------------------------------------------
+//--------------display questions---------------------------------
 
 function display(GI) {
     questionText.textContent = questions[GI].question;
@@ -68,52 +74,144 @@ function display(GI) {
     }) 
 }
 function display1(GI) {
-    questionText.textContent = questions[GI].question;
-    questions [GI].answer.forEach(function(a){
+    questionText1.textContent = questions1[GI].question1;
+    questions1 [GI].answer1.forEach(function(a){
         var button = document.createElement("button");
         button.textContent = a;
-        answerList.appendChild (button)
+        answerList1.appendChild (button)
+    }) 
+}
+function display2(GI) {
+    questionText2.textContent = questions2[GI].question2;
+    questions2 [GI].answer2.forEach(function(a){
+        var button = document.createElement("button");
+        button.textContent = a;
+        answerList2.appendChild (button)
+    }) 
+}
+function display3(GI) {
+    questionText3.textContent = questions3[GI].question3;
+    questions3 [GI].answer3.forEach(function(a){
+        var button = document.createElement("button");
+        button.textContent = a;
+        answerList3.appendChild (button)
     }) 
 }
 
-///----------------next page
+
+
+///----------------
+ 
+//button-------event
+startButton.addEventListener("click", function (e) {
+    quizContainer.style.display = "block";
+    quizContainer1.style.display = "none";
+    quizContainer2.style.display = "none";
+    quizContainer3.style.display = "none";
+    startContainer.style.display = "none";
+
+    setTimer();
+    display(0);
+    })
+    
 
 answerList.addEventListener("click", function (event) {
+    if  (questions.answer === questions.correct_answer) {
+        quizContainer.style.display = "none";
+        quizContainer1.style.display = "block";
+        display1(0);
+        
+    } else { 
+        (questions.answer === questions.correct_answer)   
+        seconds = seconds - 8;
+        }
+    })
+    
+answerList1.addEventListener("click", function (event) {
+    if  (questions1.answer1 === questions1.correct_answer1) {
+        quizContainer1.style.display = "none";
+        quizContainer2.style.display = "block";
+        display2(0);
+            
+    } else { 
+        (questions1.answer1 === questions1.correct_answer1)   
+        seconds = seconds - 8;
+         }
+    })
 
-    if (questions.answer == questions.correct_answer) {  
-       seconds = seconds - 8;
+answerList2.addEventListener("click", function (event) {
+    if  (questions2.answer2 === questions2.correct_answer2) {
+        quizContainer2.style.display = "none";
+        quizContainer3.style.display = "block";
+        display3(0);
+            
+    } else { 
+        (questions2.answer2 === questions2.correct_answer2)   
+        seconds = seconds - 8;
+         }
+    })
 
-        } else {
-            quizContainer.style.display = "none";
-            quizContainer1.style.display = "block";
-            setTimer();
-            display1(0);
+answerList3.addEventListener("click", function (event) {
+    if (questions3.answer3 === questions3.correct_answer3) {  
+        seconds = seconds - 8;
+        
+
+        } else { 
+        quizContainer3.style.display = "none";
+        endContainer.style.display = "block";
         } 
-})
+    })
 
+
+// var questions = [
+//     {
+//         question:"Commonly used data type DO not include: ",
+//         answer: ["strings", "booleans", "alerts", "numbers"], 
+//         correct_answer: "booleans",
+//     },
+//     {
+//         question:"String values must be enclosed within _____ when being assigned to variables.    ",
+//         answer: ["commas", "curly brackets", "quotes", "parenthesis"], 
+//         correct_answer: "curly brackets",
+//     },
+//     {
+//         question:"Arrays in java script can store _____.",
+//         answer: ["numbers and strings", "other arrays", "booleans", "all of the above"],  
+//         correct_answer: "all of the above",
+//     },
+//     {
+//         question:"The condition in an if /else statement is enclosed with ____.",
+//         answer: ["quotes", "curly brackets", "parenthesis", "square brackets"],  
+//         correct_answer: "parenthesis",
+//     }
+// ]
 
 var questions = [
     {
         question:"Commonly used data type DO not include: ",
         answer: ["strings", "booleans", "alerts", "numbers"], 
         correct_answer: "booleans",
-    },
+    }]
+var questions1 = [   
     {
-        question:"String values must be enclosed within _____ when being assigned to variables.    ",
-        answer: ["commas", "curly brackets", "quotes", "parenthesis"], 
-        correct_answer: "curly brackets",
-    },
+        question1:"String values must be enclosed within _____ when being assigned to variables.    ",
+        answer1: ["commas", "curly brackets", "quotes", "parenthesis"], 
+        correct_answer1: "curly brackets",
+    }]
+var questions2 = [
     {
-        question:"Arrays in java script can store _____.",
-        answer: ["numbers and strings", "other arrays", "booleans", "all of the above"],  
-        correct_answer: "all of the above",
-    },
+        question2:"Arrays in java script can store _____.",
+        answer2: ["numbers and strings", "other arrays", "booleans", "all of the above"],  
+        correct_answer2: "all of the above",
+    }]
+var questions3 = [
     {
-        question:"The condition in an if /else statement is enclosed with ____.",
-        answer: ["quotes", "curly brackets", "parenthesis", "square brackets"],  
-        correct_answer: "parenthesis",
+        question3:"The condition in an if /else statement is enclosed with ____.",
+        answer3: ["quotes", "curly brackets", "parenthesis", "square brackets"],  
+        correct_answer3: "parenthesis",
     }
 ]
+
 
 ///--------------score container
 
@@ -153,23 +251,24 @@ function storeLists() {
 
 ////---------eventlistener
 
-saveButton.addEventListener("click", function(event) {
+// saveButton.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     var textList = savedList.value.trim();
+//         if (textList === "#") {
+//         return;
+//         }
+//     lists.push(textList);
+//     textList.value = "";
     
-    event.preventDefault();
-    var textList = savedList.value.trim();
-        if (textList === "") {
-        return;
-        }
-    lists.push(textList);
-    textList.value = "";
-    
-    storeLists();
-    showScore();
-});
+//     storeLists();
+//     showScore();
+// });
 
 
 
-init();
+//init();
+
+
 //endContainer.style.display = "none";
 //scoreContainer.style.display = "block";
 
@@ -242,3 +341,26 @@ init();
     //     savedList.appendChild(li);
     // }
     // }
+
+    
+// funciton to get the next questions
+// questions appears on screen (display)
+// [ questions text]
+
+//click funciton
+//start with IF statement
+//use this keyword to target value()
+//clear hmtl for next input
+//advance to the next question, incriment
+//verify, if more questions
+//if no more question, end game else go thte
+  // function nextQuestion(next) {
+    //     index = index + next;
+    //     if (index < 0) { 
+    //       index = answerList.questions.length - 1; 
+    //     } else if (index > answerList.questions.length - 1) { 
+    //       index = 0;
+    //     }
+    //     currentQuestions = questions[index];
+    //     questions++;
+    //   }
